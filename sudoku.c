a15bece5b0aa764328869e3fce9af433eed6dd08
Replit user <>
2023-05-05 12:31:09 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -131,7 +131,7 @@ List* get_adj_nodes(Node* n){
             if (is_valid(nodito) == 1)
             {
               pushBack(list, nodito);
-              //print_node(nodito);
+              print_node(nodito);
             }
             else 
             {
