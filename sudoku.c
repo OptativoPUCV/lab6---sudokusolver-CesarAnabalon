cc2e8af26069a3977a499c52c34aabcd9dcf9101
Replit user <>
2023-05-05 12:23:18 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -93,7 +93,6 @@ int is_valid(Node* n)
         {
           num = n->sudo[i][j];
           if(num == 0) continue;
-          s = (i-r)*3 + (j-c);
           if(sub[s] == 1) return 0;
           sub[s] = 1;
         }
